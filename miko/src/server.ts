f92cab0b930137dfd86493e139588fe614fd82cd
Lunaticsatoshi <deysarkersudeep@gmail.com>
2021-06-04 18:01:10 +0530

    :sparkles: created express server

--- miko/src/server.ts
+++ miko/src/server.ts
@@ -12,8 +12,8 @@ const PORT: string | number = process.env.PORT || 5000;
 const ENV: string | number = process.env.NODE_ENV || "development";
 
 app.listen(PORT, () =>
-	console.log(
-		` 📡 Backend server: `.inverse.yellow.bold +
-			` Running in ${ENV} mode on port ${PORT}`,
-	),
+  console.log(
+    ` 📡 Backend server: `.inverse.yellow.bold +
+      ` Running in ${ENV} mode on port ${PORT}`,
+  ),
 );
