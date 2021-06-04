ca030b6f38f2602e197637074de3df162dc4384b
Lunaticsatoshi <deysarkersudeep@gmail.com>
2021-06-04 18:00:09 +0530

    :rotating_light: Fixing prettier warnings

---
+++ miko/src/server.ts
@@ -0,0 +1,19 @@
+import express, { Application, json } from "express";
+import cors from "cors";
+import "colors";
+
+const app: Application = express();
+
+app.use(cors());
+
+app.use(json());
+
+const PORT: string | number = process.env.PORT || 5000;
+const ENV: string | number = process.env.NODE_ENV || "development";
+
+app.listen(PORT, () =>
+	console.log(
+		` 📡 Backend server: `.inverse.yellow.bold +
+			` Running in ${ENV} mode on port ${PORT}`,
+	),
+);
