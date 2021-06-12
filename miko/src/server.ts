212dbf863be29b4442c02c96b169871bf6f61e08
Lunaticsatoshi <deysarkersudeep@gmail.com>
2021-06-12 20:38:42 +0530

    :package: importing app to start server

--- miko/src/server.ts
+++ miko/src/server.ts
@@ -1,12 +1,4 @@
-import express, { Application, json } from "express";
-import cors from "cors";
-import "colors";
-
-const app: Application = express();
-
-app.use(cors());
-
-app.use(json());
+import app from "./app";
 
 const PORT: string | number = process.env.PORT || 5000;
 const ENV: string | number = process.env.NODE_ENV || "development";
