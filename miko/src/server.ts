c3a779da6d6bb835b3ac9d8a3f3b6ce3acc4dca2
Lunaticsatoshi <deysarkersudeep@gmail.com>
2021-06-13 21:02:41 +0530

    :package: Adding connect db method to server

--- miko/src/server.ts
+++ miko/src/server.ts
@@ -1,7 +1,10 @@
 import app from "./app";
+import { connectDB } from "./config/db";
 
 const PORT: string | number = process.env.PORT || 5000;
 const ENV: string | number = process.env.NODE_ENV || "development";
+
+connectDB();
 
 app.listen(PORT, () =>
   console.log(
