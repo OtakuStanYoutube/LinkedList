b4215dd158db89e663da23e4513ec2a788b17aa0
Lunaticsatoshi <deysarkersudeep@gmail.com>
2021-06-13 21:17:31 +0530

    :package: Adding dotenv config to server

--- miko/src/server.ts
+++ miko/src/server.ts
@@ -1,5 +1,8 @@
 import app from "./app";
 import { connectDB } from "./config/db";
+import { config } from "dotenv";
+
+config();
 
 const PORT: string | number = process.env.PORT || 5000;
 const ENV: string | number = process.env.NODE_ENV || "development";
