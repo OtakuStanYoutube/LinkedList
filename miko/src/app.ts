e66e5303c8db82ce94913ad4ebbd0d04cdb8e9a7
Lunaticsatoshi <deysarkersudeep@gmail.com>
2021-06-14 17:28:47 +0530

    :package: adding morgan logging to server

--- miko/src/app.ts
+++ miko/src/app.ts
@@ -1,8 +1,14 @@
 import express, { Application, json } from "express";
 import cors from "cors";
+import morgan from "morgan";
+import { __prod__ } from "./constants";
 import "colors";
 
 const app: Application = express();
+
+if (!__prod__) {
+  app.use(morgan("dev"));
+}
 
 app.use(cors());
 
