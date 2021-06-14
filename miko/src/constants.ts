692871177a9b66f98f4a9d1641404cde3360754f
Lunaticsatoshi <deysarkersudeep@gmail.com>
2021-06-14 18:04:48 +0530

    :package: Adding regex for checking email

--- miko/src/constants.ts
+++ miko/src/constants.ts
@@ -1,3 +1,3 @@
 export const __prod__ = process.env.NODE_ENV === "production";
-export const regEx =
+export const emailRegEx =
   /^([0-9a-zA-Z]([-.\w]*[0-9a-zA-Z])*@([0-9a-zA-Z][-\w]*[0-9a-zA-Z]\.)+[a-zA-Z]{2,9})$/;
