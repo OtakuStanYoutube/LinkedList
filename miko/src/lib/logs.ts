c4ed0a3e5dcf94b6b0541432d26da10262a9e6d8
Lunaticsatoshi <deysarkersudeep@gmail.com>
2021-06-14 19:00:49 +0530

    :paintbrush: adding logs

---
+++ miko/src/lib/logs.ts
@@ -0,0 +1,15 @@
+export const ready = (message: string) => {
+  console.log(message);
+};
+
+export const errors = (message: string) => {
+  console.error(message);
+};
+
+export const event = (message: string) => {
+  console.log(message);
+};
+
+export const warn = (message: string) => {
+  console.log(message);
+};
