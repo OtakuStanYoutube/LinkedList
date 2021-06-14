20fb783ba2f2907455675f99f6c71b64797cfc8f
Lunaticsatoshi <deysarkersudeep@gmail.com>
2021-06-14 17:38:37 +0530

    :package: adding error handeler middleware to app

--- miko/src/app.ts
+++ miko/src/app.ts
@@ -2,6 +2,7 @@ import express, { Application, json } from "express";
 import cors from "cors";
 import morgan from "morgan";
 import { __prod__ } from "./constants";
+import { notFound, errorHandler } from "./middlewares/error";
 import "colors";
 
 const app: Application = express();
@@ -13,5 +14,8 @@ if (!__prod__) {
 app.use(cors());
 
 app.use(json());
+
+app.use(notFound);
+app.use(errorHandler);
 
 export default app;
