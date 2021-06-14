23776e63e16849d413ba99890d61f571545791e1
Lunaticsatoshi <deysarkersudeep@gmail.com>
2021-06-14 19:20:49 +0530

    :package: updating server file

--- miko/src/server.ts
+++ miko/src/server.ts
@@ -1,9 +1,9 @@
 import { config } from "dotenv";
+config();
+
 import app from "./app";
 import { connectDB } from "./config/db";
 import { ready } from "./lib/logs";
-
-config();
 
 const PORT: string | number = process.env.PORT || 5000;
 const ENV: string | number = process.env.NODE_ENV || "development";
