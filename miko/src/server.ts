b264e0c14bff82a5e82a19b00b57441a4c1cb00f
Lunaticsatoshi <deysarkersudeep@gmail.com>
2021-06-14 19:06:07 +0530

    :package: adding error logs to error server

--- miko/src/server.ts
+++ miko/src/server.ts
@@ -1,8 +1,9 @@
 import { config } from "dotenv";
-config();
-
 import app from "./app";
 import { connectDB } from "./config/db";
+import { ready } from "./lib/logs";
+
+config();
 
 const PORT: string | number = process.env.PORT || 5000;
 const ENV: string | number = process.env.NODE_ENV || "development";
@@ -10,7 +11,7 @@ const ENV: string | number = process.env.NODE_ENV || "development";
 connectDB();
 
 app.listen(PORT, () =>
-  console.log(
+  ready(
     ` Backend server: `.inverse.yellow.bold +
       ` Running in ${ENV} mode on port ${PORT}`,
   ),
