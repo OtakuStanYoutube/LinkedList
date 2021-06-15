ac68a66d16eab9ca08438f370eb822712e2e0cc2
Lunaticsatoshi <deysarkersudeep@gmail.com>
2021-06-15 19:29:34 +0530

    :package: configuring cors and cookieparser in server

--- miko/src/app.ts
+++ miko/src/app.ts
@@ -1,6 +1,7 @@
 import express, { Application, json } from "express";
 import cors from "cors";
 import morgan from "morgan";
+import cookieParser from "cookie-parser";
 import { __prod__ } from "./constants";
 import { notFound, errorHandler } from "./middlewares/error";
 import "colors";
@@ -11,9 +12,25 @@ if (!__prod__) {
   app.use(morgan("dev"));
 }
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    maxAge: __prod__ ? 86400 : undefined,
+    allowedHeaders: ["Content-Type", "Authorization"],
+    exposedHeaders: [
+      "access-token",
+      "refresh-token",
+      "content-type",
+      "content-length",
+    ],
+  }),
+);
 
 app.use(json());
+
+app.use(cookieParser());
+
+app.set("trust proxy", 1);
 
 app.use(notFound);
 app.use(errorHandler);
