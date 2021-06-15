2cc8cc271fe15f13c067ce7df9a2e81b9bb4d245
Lunaticsatoshi <deysarkersudeep@gmail.com>
2021-06-15 20:50:25 +0530

    :package: Adding regex to check uswrname and image urls

--- miko/src/constants.ts
+++ miko/src/constants.ts
@@ -1,3 +1,11 @@
 export const __prod__ = process.env.NODE_ENV === "production";
 export const emailRegEx =
   /^([0-9a-zA-Z]([-.\w]*[0-9a-zA-Z])*@([0-9a-zA-Z][-\w]*[0-9a-zA-Z]\.)+[a-zA-Z]{2,9})$/;
+
+// export const usernameRegEx = /^(?=[a-zA-Z0-9._]{8,20}$)(?!.*[_.]{2})[^_.].*[^_.]$/;
+
+export const usernameRegEx =
+  /^(?=.{3,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$/;
+
+export const imgUrlRegEx =
+  /(http)?s?:?(\/\/[^"']*\.(?:png|jpg|jpeg|gif|png|svg))/;
