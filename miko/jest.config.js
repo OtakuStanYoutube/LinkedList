8feb6ca64ff52c022103f9b5578cc6fb7992377f
Lunaticsatoshi <deysarkersudeep@gmail.com>
2021-06-18 16:22:53 +0530

    :wrench: Adding configuration for jest

--- miko/jest.config.js
+++ miko/jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   clearMocks: true,
   moduleFileExtensions: ["js"],
-  roots: ["<rootDir>/dist"],
+  roots: ["./dist"],
   testEnvironment: "node",
   coveragePathIgnorePatterns: ["/node_modules/"],
 };
