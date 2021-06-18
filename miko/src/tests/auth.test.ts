d870dee7fc5af3a55b5a92de079ad071af2219c8
Lunaticsatoshi <deysarkersudeep@gmail.com>
2021-06-18 17:10:29 +0530

    :test_tube: Testing api routes for user

--- miko/src/tests/auth.test.ts
+++ miko/src/tests/auth.test.ts
@@ -14,3 +14,46 @@ describe("Post Endpoints", () => {
     expect(res.body.message).toBeDefined();
   });
 });
+
+describe("POST /api/v1/users/login", () => {
+  connectDB();
+  describe("Given an email and password", () => {
+    test("should respond with a 201 status code", async () => {
+      const res = await request(app).post("/api/v1/users/login").send({
+        email: "test@test.com",
+        password: "test1234",
+      });
+
+      expect(res.statusCode).toEqual(201);
+    });
+
+    test("should return a json response", async () => {
+      const res = await request(app).post("/api/v1/users/login").send({
+        email: "test@test.com",
+        password: "test1234",
+      });
+
+      expect(res.headers["content-type"]).toEqual(
+        expect.stringContaining("json"),
+      );
+    });
+
+    test("response should have a user id", async () => {
+      const res = await request(app).post("/api/v1/users/login").send({
+        email: "test@test.com",
+        password: "test1234",
+      });
+
+      expect(res.body).toHaveProperty("id");
+    });
+
+    test("response should have a defined user id", async () => {
+      const res = await request(app).post("/api/v1/users/login").send({
+        email: "test@test.com",
+        password: "test1234",
+      });
+
+      expect(res.body.id).toBeDefined();
+    });
+  });
+});
