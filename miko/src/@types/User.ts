d07572aca5a3d1042f2e94d5522f493e716f7b10
Lunaticsatoshi <deysarkersudeep@gmail.com>
2021-06-19 14:08:51 +0530

    :label: Updating user type

--- miko/src/@types/User.ts
+++ miko/src/@types/User.ts
@@ -4,7 +4,6 @@ export interface IUser extends Document {
   username: string;
   email: string;
   password: string;
-  bio?: string;
   imgUrl: string;
   isAdmin?: boolean;
   matchPassword: (password: string) => string;
