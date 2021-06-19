39baa618a7f07f7195a3f4e03036367c657744ae
Lunaticsatoshi <deysarkersudeep@gmail.com>
2021-06-19 19:39:26 +0530

    :bricks: Adding time stamps to Profile model

--- miko/src/models/Profile.model.ts
+++ miko/src/models/Profile.model.ts
@@ -1,52 +1,55 @@
 import { model, Schema } from "mongoose";
 import { IProfile } from "../@types/Profile";
 
-const profileSchema = new Schema<IProfile>({
-  handle: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  bio: {
-    type: String,
-    required: false,
-    default: "",
-  },
-  imgUrl: {
-    type: String,
-    required: true,
-    default: "https://via.placeholder.com/200/000000/FFFFFF/?text=LL",
-  },
-  parent: {
-    type: Schema.Types.ObjectId,
-    ref: "Profile",
-    required: true,
-  },
-  social: [
-    {
-      icon: String,
-      link: String,
-      alt: String,
+const profileSchema = new Schema<IProfile>(
+  {
+    handle: {
+      type: String,
+      required: true,
+      unique: true,
     },
-  ],
-  visibility: {
-    type: String,
-    default: "unpublished",
-    required: true,
-    enum: ["unpublished", "published", "published-18+"],
+    bio: {
+      type: String,
+      required: false,
+      default: "",
+    },
+    imgUrl: {
+      type: String,
+      required: true,
+      default: "https://via.placeholder.com/200/000000/FFFFFF/?text=LL",
+    },
+    parent: {
+      type: Schema.Types.ObjectId,
+      ref: "Profile",
+      required: true,
+    },
+    social: [
+      {
+        icon: String,
+        link: String,
+        alt: String,
+      },
+    ],
+    visibility: {
+      type: String,
+      default: "unpublished",
+      required: true,
+      enum: ["unpublished", "published", "published-18+"],
+    },
+    custom_css: String,
+    custom_html: String,
+    theme: {
+      type: Schema.Types.ObjectId,
+      ref: "Theme",
+      required: false,
+    },
+    views: {
+      type: Number,
+      required: false,
+      default: 0,
+    },
   },
-  custom_css: String,
-  custom_html: String,
-  theme: {
-    type: Schema.Types.ObjectId,
-    ref: "Theme",
-    required: false,
-  },
-  views: {
-    type: Number,
-    required: false,
-    default: 0,
-  },
-});
+  { timestamps: true },
+);
 
 export const Profile = model<IProfile>("Profile", profileSchema);
