e417a9f3514f9d4cfc32f1e5d15f3d15070d7971
Lunaticsatoshi <deysarkersudeep@gmail.com>
2021-06-22 19:44:26 +0530

    :package: Google OAuth routes

--- miko/src/routes/auth.ts
+++ miko/src/routes/auth.ts
@@ -6,10 +6,10 @@ export default (passport: PassportStatic): Router => {
   const authRouter = Router();
 
   // OAuth Providers
-  //   if (process.env.OASIS_API_GITHUB_CLIENT_ID) authRouter.use("/github", GitHubAuth(passport));
-  //   if (process.env.OASIS_API_TWITTER_KEY)
+  //   if (process.env.LINKEDLIST_API_GITHUB_CLIENT_ID) authRouter.use("/github", GitHubAuth(passport));
+  //   if (process.env.LINKEDLIST_API_TWITTER_KEY)
   //     authRouter.use("/twitter", TwitterAuth(passport));
-  //   if (process.env.OASIS_API_DISCORD_CLIENT_ID)
+  //   if (process.env.LINKEDLIST_API_DISCORD_CLIENT_ID)
   //     authRouter.use("/discord", DiscordAuth(passport));
   if (process.env.LINKEDLIST_API_GOOGLE_CLIENT_ID) {
     authRouter.use("/google", GoogleAuth(passport));
