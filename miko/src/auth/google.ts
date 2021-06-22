28414ece5ad213cae59e4f3edada851576993ea5
Lunaticsatoshi <deysarkersudeep@gmail.com>
2021-06-22 14:09:51 +0530

    :package: Creating google strategy

---
+++ miko/src/auth/google.ts
@@ -0,0 +1,43 @@
+import { Router, Request, Response } from "express";
+import { Strategy } from "passport-google-oauth20";
+import { User } from "../models";
+import { PassportStatic } from "passport";
+
+export default (passport: PassportStatic): Router => {
+  passport.use(
+    new Strategy(
+      {
+        clientID: process.env.LINKEDLIST_API_GOOGLE_CLIENT_ID!,
+        clientSecret: process.env.LINKEDLIST_API_GOOGLE_CLIENT_SECRET!,
+        callbackURL: process.env.LINKEDLIST_API_GOOGLE_CALLBACK_URL!,
+      },
+      async (_, __, profile, done) => {
+        const id = String(profile.id);
+        const user = await User.findOne({ where: { google: id } });
+
+        console.log(profile);
+        console.log(user);
+      },
+    ),
+  );
+
+  const router = Router();
+
+  router.get(
+    "/",
+    passport.authenticate("google", {
+      scope: ["https://www.googleapis.com/auth/userinfo.profile"],
+      session: false,
+    }),
+  );
+
+  router.get(
+    "/callback",
+    passport.authenticate("google", (req: Request, res: Response) => {
+      console.log(req.user);
+      res.status(201);
+    }),
+  );
+
+  return router;
+};
