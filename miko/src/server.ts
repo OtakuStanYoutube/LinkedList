a0c46f98798fe060c3c684d592209def0cf6e16e
Lunaticsatoshi <deysarkersudeep@gmail.com>
2021-06-24 17:08:09 +0530

    :package: Connecting to redis in server

--- miko/src/server.ts
+++ miko/src/server.ts
@@ -3,12 +3,14 @@ config();
 
 import app from "./app";
 import { connectDB } from "./config/db";
+import { connectRedis } from "./config/redis_connect";
 import { ready } from "./lib/logs";
 
 const PORT: string | number = process.env.PORT || 5000;
 const ENV: string | number = process.env.NODE_ENV || "development";
 
 connectDB();
+connectRedis();
 
 app.listen(PORT, () =>
   ready(
