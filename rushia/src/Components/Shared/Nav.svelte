d6e08cb1599897fce34f4079c79132801bc1797c
agarwal222 <agarwalutkarsh222@gmail.com>
2021-06-28 17:03:43 +0530

    :lipstick: Added Nav functionality

--- rushia/src/Components/Shared/Nav.svelte
+++ rushia/src/Components/Shared/Nav.svelte
@@ -1,8 +1,18 @@
+<script lang="ts">
+  let curr: string = "login";
+</script>
+
 <nav class="navigation">
-  <li>
-    <a class="link active" href="/">Log In</a>
-  </li>
-  <li><a class="link" href="/signup">Sign Up</a></li>
+  <a
+    class="link {curr == 'login' ? 'active' : ''}"
+    on:click={() => (curr = "login")}
+    href="/">Log In</a
+  >
+  <a
+    class="link {curr == 'signup' ? 'active' : ''}"
+    on:click={() => (curr = "signup")}
+    href="/signup">Sign Up</a
+  >
 </nav>
 
 <style lang="scss">
@@ -15,6 +25,10 @@
       // background-color: black;
       padding: 0.3em 1.5em;
       margin-right: 0.5em;
+    }
+
+    .link:hover {
+      background-color: var(--main-bg-color);
     }
 
     .active {
