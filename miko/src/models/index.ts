0619016369131159330428b87c7e282ff8c7dfb5
Lunaticsatoshi <deysarkersudeep@gmail.com>
2021-07-05 19:48:47 +0530

    :bricks: Exporting Token model

--- miko/src/models/index.ts
+++ miko/src/models/index.ts
@@ -1,4 +1,5 @@
 import { User } from "./User.model";
 import { Profile } from "./Profile.model";
+import { Token } from "./Token.model";
 
-export { User, Profile };
+export { User, Profile, Token };
