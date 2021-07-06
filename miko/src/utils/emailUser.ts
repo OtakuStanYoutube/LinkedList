e98221ed6357ba3ee6c82cb24bc96fb74d39752a
Lunaticsatoshi <deysarkersudeep@gmail.com>
2021-07-06 21:13:09 +0530

    :triangular_ruler: Adding email config in email utility

---
+++ miko/src/utils/emailUser.ts
@@ -0,0 +1,28 @@
+import { transporter } from "../config/mailer";
+import { baseURL } from "../constants";
+
+export const mailUser = async (
+  username: string,
+  email: string,
+  token: string,
+) => {
+  const verifyUrl = `${baseURL}/api/v1/user/verifyEmail/${token}`;
+  const mailOptions = {
+    from: "lolicon@hentaihaven.com",
+    to: email,
+    subject: "Account Verification",
+    text: `Hello ${username}, Welcome to LinkedList. Click on the link below to verify your account \n\n ${verifyUrl}`,
+  };
+
+  const info = await transporter.sendMail(mailOptions, (error, message) => {
+    if (error) {
+      return "Technical Issue!, There was some error in sending the email. Please click on resend for verify your Email.";
+    } else {
+      console.log(message);
+
+      return "Mail Sent Successfully";
+    }
+  });
+
+  return info;
+};
