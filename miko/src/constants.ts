8a0f6745a01c52991dad63cf6a98ca703cd2d853
Lunaticsatoshi <deysarkersudeep@gmail.com>
2021-07-06 21:03:55 +0530

    :package: creating base url constant

--- miko/src/constants.ts
+++ miko/src/constants.ts
@@ -1,4 +1,5 @@
 export const __prod__ = process.env.NODE_ENV === "production";
+export const baseURL = process.env.BASE_URI;
 export const emailRegEx =
   /^([0-9a-zA-Z]([-.\w]*[0-9a-zA-Z])*@([0-9a-zA-Z][-\w]*[0-9a-zA-Z]\.)+[a-zA-Z]{2,9})$/;
 
