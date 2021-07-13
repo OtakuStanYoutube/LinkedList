ab370b120ff726c5ed768f5bd712b26157d6d152
Lunaticsatoshi <deysarkersudeep@gmail.com>
2021-07-13 17:36:14 +0530

    :wrench: Updating next config for eslint rules

--- ayame/next.config.js
+++ ayame/next.config.js
@@ -1,3 +1,6 @@
 module.exports = {
-  reactStrictMode: true,
-}
+	reactStrictMode: true,
+	eslint: {
+		dirs: ["pages", "utils", "components", "store", "hooks"], // Only run ESLint on the 'pages' and 'utils' directories during production builds (next build)
+	},
+};
