57a7f5b5b896acb760da09bf566f883a7e49e8be
Lunaticsatoshi <deysarkersudeep@gmail.com>
2021-07-18 20:51:41 +0530

    :package: Creating wrapper component

---
+++ ayame/src/components/Wrapper.tsx
@@ -0,0 +1,21 @@
+import { FC } from "react";
+import styles from "../../styles/Home.module.scss";
+
+export type WrapperVariant = "small" | "regular";
+
+interface WrapperProps {
+  variant?: WrapperVariant;
+}
+
+export const Wrapper: FC<WrapperProps> = ({
+  children,
+  variant = "regular",
+}) => {
+  return (
+    <div
+      className={variant === "regular" ? styles.container : styles.container}
+    >
+      {children}
+    </div>
+  );
+};
