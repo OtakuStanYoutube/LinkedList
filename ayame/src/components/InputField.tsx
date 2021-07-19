3205ac1be87fba7bae9b56d3b3616612a8dcee37
Lunaticsatoshi <deysarkersudeep@gmail.com>
2021-07-19 20:59:02 +0530

    :lipstick: renaming wrapper styles

--- ayame/src/components/InputField.tsx
+++ ayame/src/components/InputField.tsx
@@ -1,6 +1,8 @@
 import { InputHTMLAttributes, FC } from "react";
 import { useField } from "formik";
 
+import { TextField, InputAdornment, FormControlLabel } from "@material-ui/core";
+
 type InputFieldProps = InputHTMLAttributes<HTMLInputElement> & {
   label: string;
   name: string;
@@ -16,20 +18,16 @@ const InputField: FC<InputFieldProps> = ({
   const [field, { error }] = useField(props);
   return (
     <div className="mb-4">
-      <label
-        className="block mb-2 text-sm font-bold text-gray-700"
-        htmlFor={field.name}
-      >
-        {field.name}
-      </label>
-      <input
-        className="w-full px-3 py-2 mb-3 text-sm leading-tight text-gray-700 border border-red-500 rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+      <TextField
+        fullWidth
+        autoComplete="username"
         id={field.name}
         {...field}
-        {...props}
-        // placeholder="******************"
+        // {...props}
+        variant="standard"
+        error={error ? true : false}
+        helperText={error}
       />
-      {error ? <p className="text-xs italic text-red-500">{error}</p> : null}
     </div>
   );
 };
