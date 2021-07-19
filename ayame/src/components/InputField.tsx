37edfd23c530711fde39bb65cf0fd7b392265d9a
Lunaticsatoshi <deysarkersudeep@gmail.com>
2021-07-19 21:31:34 +0530

    :lipstick: designed input field

--- ayame/src/components/InputField.tsx
+++ ayame/src/components/InputField.tsx
@@ -1,8 +1,6 @@
 import { InputHTMLAttributes, FC } from "react";
 import { useField } from "formik";
 
-import { TextField, InputAdornment, FormControlLabel } from "@material-ui/core";
-
 type InputFieldProps = InputHTMLAttributes<HTMLInputElement> & {
   label: string;
   name: string;
@@ -17,17 +15,21 @@ const InputField: FC<InputFieldProps> = ({
 }) => {
   const [field, { error }] = useField(props);
   return (
-    <div className="mb-4">
-      <TextField
-        fullWidth
-        autoComplete="username"
+    <div className="mb-4 w-full">
+      <label
+        className="block mb-2 text-sm font-bold text-gray-700"
+        htmlFor={field.name}
+      >
+        {field.name}
+      </label>
+      <input
+        className="border-rounded w-full py-2 px-4 outline-none leading-tight text-gray-700 shadow appearance-none focus:outline-none focus:shadow-outline"
         id={field.name}
         {...field}
-        // {...props}
-        variant="standard"
-        error={error ? true : false}
-        helperText={error}
+        {...props}
+        // placeholder="******************"
       />
+      {error ? <p className="text-xs italic text-red-500">{error}</p> : null}
     </div>
   );
 };
