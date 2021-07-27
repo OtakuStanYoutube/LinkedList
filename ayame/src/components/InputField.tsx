6639616be3d9a7ebded9632b6e28651e36f941b8
Lunaticsatoshi <deysarkersudeep@gmail.com>
2021-07-27 20:34:21 +0530

    :lipstick: Designed the textfield

--- ayame/src/components/InputField.tsx
+++ ayame/src/components/InputField.tsx
@@ -19,19 +19,6 @@ const InputField: FC<InputFieldProps> = ({
   const [field, { error }] = useField(props);
   return (
     <div className="mb-4 w-full">
-      {/* <label
-        className="block mb-2 text-sm font-bold text-gray-700"
-        htmlFor={field.name}
-      >
-        {field.name}
-      </label> */}
-      {/* <input
-        className="border-rounded w-full py-2 px-4 outline-none leading-tight text-gray-700 shadow appearance-none focus:outline-none focus:shadow-outline"
-        id={field.name}
-        {...field}
-        {...props}
-        // placeholder="******************"
-      /> */}
       <TextField
         id={field.name}
         label={field.name}
@@ -42,7 +29,6 @@ const InputField: FC<InputFieldProps> = ({
         helperText={error}
         variant="outlined"
       />
-      {error ? <p className="text-xs italic text-red-500">{error}</p> : null}
     </div>
   );
 };
