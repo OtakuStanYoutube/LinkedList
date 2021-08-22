defcb42135a585f8d54c35dc80bcc34c15a1e931
Lunaticsatoshi <deysarkersudeep@gmail.com>
2021-08-22 20:11:02 +0530

    :hammer: Updating DB config

--- miko/src/config/db.ts
+++ miko/src/config/db.ts
@@ -17,7 +17,11 @@ export const connectDB = async () => {
   try {
     const con = await createConnection({
       type: "postgres",
-      // database: __prod__ ? undefined : "linkedlist",
+      host: __prod__ ? process.env.DB_HOST : "host.docker.internal",
+      port: 5432,
+      username: __prod__ ? process.env.USERNAME : "postgres",
+      password: __prod__ ? process.env.DB_PASSWORD : "satoshi",
+      database: __prod__ ? process.env.DB_DATABASE : "linkedlist",
       // url: __prod__ ? process.env.DATABASE_URL : undefined,
       entities: [join(__dirname, "./entities/*")],
       migrations: [join(__dirname, "./migrations/*")],
