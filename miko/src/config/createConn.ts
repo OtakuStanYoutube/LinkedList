5835266732b9eff44d0b8329d1b949f0d6592b4c
Lunaticsatoshi <deysarkersudeep@gmail.com>
2021-08-23 19:41:34 +0530

    :hammer: Updating database connection postgres

--- miko/src/config/createConn.ts
+++ miko/src/config/createConn.ts
@@ -5,11 +5,11 @@ import { __prod__ } from "../constants";
 export const createConn = () =>
   createConnection({
     type: "postgres",
-    host: __prod__ ? process.env.DB_HOST : "host.docker.internal",
-    port: 5432,
-    username: __prod__ ? process.env.USERNAME : "postgres",
-    password: __prod__ ? process.env.DB_PASSWORD : "satoshi",
-    database: __prod__ ? process.env.DB_DATABASE : "linkedlist",
+    // host: __prod__ ? process.env.DB_HOST : "host.docker.internal",
+    // port: 5432,
+    // username: __prod__ ? process.env.USERNAME : "postgres",
+    // password: __prod__ ? process.env.DB_PASSWORD : "satoshi",
+    // database: __prod__ ? process.env.DB_DATABASE : "linkedlist",
     // url: __prod__ ? process.env.DATABASE_URL : undefined,
     entities: [join(__dirname, "./entities/*")],
     migrations: [join(__dirname, "./migrations/*")],
--- miko/src/server.ts
+++ miko/src/server.ts
@@ -3,18 +3,27 @@ config();
 
 import app from "./app";
 import { connectDB } from "./config/db";
+import { createConn } from "./config/createConn";
 import { connectRedis } from "./config/redis_connect";
 import { ready } from "./lib/logs";
 
 const PORT: string | number = process.env.PORT || 5000;
 const ENV: string | number = process.env.NODE_ENV || "development";
 
-connectDB();
-connectRedis();
+const main = async () => {
+  connectDB();
+  connectRedis();
+  const conn = await createConn();
 
-app.listen(PORT, () =>
-  ready(
-    ` Backend server: `.inverse.yellow.bold +
-      ` Running in ${ENV} mode on port ${PORT}`,
-  ),
-);
+  console.log("connected, running migrations now");
+
+  await conn.runMigrations();
+  console.log("migrations ran");
+
+  app.listen(PORT, () =>
+    ready(
+      ` Backend server: `.inverse.yellow.bold +
+        ` Running in ${ENV} mode on port ${PORT}`,
+    ),
+  );
+};
