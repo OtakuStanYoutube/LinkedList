da5544b2750e4de7aa70cab0085993eb84dabe96
Lunaticsatoshi <deysarkersudeep@gmail.com>
2021-08-23 19:29:47 +0530

    :hammer: Updating mongo DB connection config

--- miko/src/config/db.ts
+++ miko/src/config/db.ts
@@ -1,43 +1,26 @@
-import { join } from "path";
-import { createConnection } from "typeorm";
-// import { connect, disconnect } from "mongoose";
+import { connect, disconnect } from "mongoose";
 import { event, errors } from "../lib/logs";
-import { __prod__ } from "../constants";
 
-// const mongoURI: string = process.env.MONGO_URI!;
+const mongoURI: string = process.env.MONGO_URI!;
 
-// const options = {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-//   useFindAndModify: false,
-//   useCreateIndex: true,
-// };
+const options = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true,
+};
 
 export const connectDB = async () => {
   try {
-    const con = await createConnection({
-      type: "postgres",
-      host: __prod__ ? process.env.DB_HOST : "host.docker.internal",
-      port: 5432,
-      username: __prod__ ? process.env.USERNAME : "postgres",
-      password: __prod__ ? process.env.DB_PASSWORD : "satoshi",
-      database: __prod__ ? process.env.DB_DATABASE : "linkedlist",
-      // url: __prod__ ? process.env.DATABASE_URL : undefined,
-      entities: [join(__dirname, "./entities/*")],
-      migrations: [join(__dirname, "./migrations/*")],
-      synchronize: !__prod__,
-      logging: !__prod__,
-    });
+    const con = await connect(mongoURI, options);
 
     event(` MongoDB Connected: `.inverse.green + ` ${con}`);
-
-    return con;
   } catch (error) {
     errors(` ! Error: `.inverse.red.bold + `${error.message}`);
     throw error;
   }
 };
 
-// export const connectClose = async () => {
-//   await disconnect();
-// };
+export const connectClose = async () => {
+  await disconnect();
+};
