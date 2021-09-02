96511d5f71f1ac3b50df22fbcf9e181948b4ec40
Lunaticsatoshi <deysarkersudeep@gmail.com>
2021-09-02 18:30:29 +0530

    :bricks: Updating user model to support user roles enums

--- miko/src/entities/User.ts
+++ miko/src/entities/User.ts
@@ -1,9 +1,11 @@
 import { Entity, Column, Index, BeforeInsert } from "typeorm";
-import { Exclude } from 'class-transformer';
-import { IsEmail, Length } from 'class-validator';
+import { Exclude } from "class-transformer";
+import { IsEmail, Length } from "class-validator";
 import { hash } from "argon2";
+import { v4 as uuidv4 } from "uuid";
 
 import BaseModel from "./BaseModel";
+import { UserRole } from "../enums/userRole";
 
 @Entity("users")
 export default class User extends BaseModel {
@@ -11,6 +13,10 @@ export default class User extends BaseModel {
     super();
     Object.assign(this, user);
   }
+
+  @Column({ type: "uuid", unique: true })
+  @Index()
+  userID: string;
 
   @Index()
   @IsEmail(undefined, { message: "Must be a valid email address" })
@@ -28,8 +34,27 @@ export default class User extends BaseModel {
   @Length(6, 255, { message: "Must be at least 6 characters long" })
   password: string;
 
+  @Column({ default: "" })
+  oAuthId: string;
+
+  @Column({ default: "" })
+  tokenId: string;
+
+  @Column({ default: "https://via.placeholder.com/200/000000/FFFFFF/?text=LL" })
+  imgUrl: string;
+
+  @Column({ default: UserRole.USER, enum: UserRole })
+  role: string;
+
+  @Column({ default: false, type: "boolean" })
+  isActive: boolean;
+
+  @Column({ default: false, type: "boolean" })
+  isVerified: boolean;
+
   @BeforeInsert()
   async hashPassword() {
+    this.userID = uuidv4();
     this.password = await hash(this.password);
   }
 }
