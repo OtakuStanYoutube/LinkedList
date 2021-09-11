09752a9f70fb8647d6c20612f54e49ac8edad0bb
Lunaticsatoshi <deysarkersudeep@gmail.com>
2021-09-11 18:55:51 +0530

    :books: Creating default exports for enum

---
+++ miko/src/enums/index.ts
@@ -0,0 +1,11 @@
+import { UserRole } from "./userRole";
+import { ProfileType } from "./profileType";
+import { Layout } from "./Layout";
+import { Shape } from "./Shape";
+
+export default {
+  UserRole,
+  ProfileType,
+  Layout,
+  Shape,
+};
