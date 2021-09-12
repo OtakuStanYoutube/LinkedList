759c79ba36805687bbea8b31c0ac63b96b709703
Lunaticsatoshi <deysarkersudeep@gmail.com>
2021-09-12 19:58:39 +0530

    :gear: Updating user controller

--- miko/src/controllers/user.ts
+++ miko/src/controllers/user.ts
@@ -1,18 +1,25 @@
 import { Request, Response } from "express";
 import { v4 as uuidv4 } from "uuid";
-import { User, Profile, Token } from "../models";
 import { randomBytes } from "crypto";
-import {
-  generateAccessToken,
-  generateRefreshToken,
-} from "../utils/generateToken";
+import { validate } from "class-validator";
+import { verify } from "argon2";
+
+// Entities
+import User from "../entities/User";
+import Profile from "../entities/Profile";
+
+// Utilities
+import { generateTokens } from "../utils/generateToken";
 import { generateUniqueUsername } from "../utils/generateUniqueUsername";
-import { mailUser } from "../utils/emailUser";
+import { mapErrors } from "../utils/mapErrors";
+// import { mailUser } from "../utils/emailUser";
 import {
   validateLoginInput,
   validateRegisterInput,
 } from "../utils/vaidateInput";
 import { __prod__ } from "../constants";
+
+import { redisClient } from "../config/redis_connect";
 
 export const loginUser = async (req: Request, res: Response) => {
   const { email, password } = req.body;
@@ -20,20 +27,25 @@ export const loginUser = async (req: Request, res: Response) => {
   const { errors, valid } = validateLoginInput(email, password);
 
   if (!valid) {
-    res.status(401);
-    throw new Error(`❗ Errors ${errors.email} & ${errors.password}`);
+    res.status(401).json({ errors });
   }
 
   const user = await User.findOne({ email });
 
-  if (user && (await user.matchPassword(password))) {
-    const tokenId = uuidv4();
+  if (user && (await verify(user.password, password))) {
+    let newTokenId = uuidv4();
 
-    user.tokenId = tokenId;
+    if (user.tokenId) {
+      newTokenId = user.tokenId;
+    }
+
+    user.tokenId = newTokenId;
     await user.save();
 
-    const accessToken = generateAccessToken(user._id, tokenId);
-    const refreshToken = generateRefreshToken(user._id);
+    const { accessToken, refreshToken } = generateTokens(
+      user.userID,
+      newTokenId,
+    );
 
     res.cookie("jwt", accessToken, {
       httpOnly: true,
@@ -46,8 +58,7 @@ export const loginUser = async (req: Request, res: Response) => {
       secure: __prod__,
     });
     res.status(201).json({
-      id: user._id,
-      isAdmin: user.isAdmin,
+      user,
       accessToken,
     });
   } else {
@@ -62,98 +73,87 @@ export const registerUser = async (req: Request, res: Response) => {
   const { errors, valid } = validateRegisterInput(username, email, password);
 
   if (!valid) {
-    res.status(401);
-    throw new Error(
-      `❗ Errors ${errors.email} & ${errors.password} & ${errors.username}`,
-    );
+    res.status(401).json({ errors });
   }
 
-  const userExists = await User.findOne({ username });
+  const userExists = await User.findOne({ email });
+  const usernameExists = await User.findOne({ username });
 
   if (userExists) {
-    res.status(400).json({
-      message: "❗ User Already Registered!",
-      generatedUsername: generateUniqueUsername(username),
+    res.status(401).json({
+      status: false,
+      message: "❗ User already exists",
     });
-    throw new Error("❗ User Already Registered!");
   }
 
-  const user = await User.create({ username, email, password });
+  if (usernameExists) {
+    res.status(401).json({
+      status: false,
+      message: "❗ Username already taken! Please select a new username",
+      generatedUsername: generateUniqueUsername(username),
+    });
+  }
+
+  const user = new User({ username, email, password });
+
+  const generatedErrors = await validate(user);
+
+  if (generatedErrors.length > 0) {
+    res.status(401).json({
+      status: false,
+      message: "❗ Invalid User",
+      errors: mapErrors(generatedErrors),
+    });
+  }
+
+  await user.save();
 
   if (user) {
-    const profile = await Profile.create({
-      parent: user._id,
-      handle: user.username,
-    });
+    const profile = new Profile({ displayname: username, user });
 
-    const token = await Token.create({
-      userId: user._id,
-      token: randomBytes(10).toString("hex"),
-    });
+    await profile.save();
 
-    user.activeProfile = profile._id;
-    await user.save();
+    const activationToken = randomBytes(10).toString("hex");
 
-    const info = await mailUser(username, email, token._id);
+    redisClient.setex(
+      activationToken.toString(),
+      1200,
+      JSON.stringify({ id: user.userID }),
+    );
+
+    // const info = await mailUser(username, email, activationToken._id);
 
     res.status(201).json({
       status: true,
-      message: info,
+      message: "User created Sucessfully",
     });
   } else {
     throw new Error("❗ Invalid User!");
   }
 };
 
-export const getAccessToken = async (req: Request, res: Response) => {
-  const user = await User.findById(req.body.id);
+export const verifyEmail = (req: Request, res: Response) => {
+  const { activationToken } = req.params;
 
-  if (user) {
-    const tokenId = uuidv4();
-
-    user.tokenId = tokenId;
-    await user.save();
-
-    const accessToken = generateAccessToken(user._id, tokenId);
-    const refreshToken = generateRefreshToken(user._id);
-
-    res.cookie("jwt", accessToken, {
-      httpOnly: true,
-      path: "/access_token",
-      secure: __prod__,
-    });
-    res.cookie("jwt_refresh", refreshToken, {
-      httpOnly: true,
-      path: "/refresh_token",
-      secure: __prod__,
-    });
-
-    res.status(201).json({
-      id: user._id,
-      isAdmin: user.isAdmin,
-      accessToken,
-      refreshToken,
-    });
-  } else {
-    res.status(401);
-    throw new Error("❗ Invalid User");
-  }
-};
-
-export const verifyEmail = async (req: Request, res: Response) => {
-  const { token } = req.params;
-
-  const userToken = await Token.findById(token);
-
-  if (userToken) {
-    const user = await User.findById(userToken.userId);
-
-    if (user) {
-      user.isActive = true;
-      await user.save();
+  redisClient.get(activationToken.toString(), async (err, data) => {
+    if (err) {
+      throw new Error("❗ Token has expired");
     }
-  } else {
-    res.status(401).json({ status: false, message: "Token has expired" });
-    throw new Error("❗ Token has expired");
-  }
+
+    if (data) {
+      const { id } = JSON.parse(data);
+
+      const user = await User.findOne({ userID: id });
+
+      if (user) {
+        user.isActive = true;
+        await user.save();
+
+        res.status(200).json({
+          status: true,
+          message: "Email Verified",
+        });
+      }
+    }
+  });
 };
