b6c9fab09f49710a31aff8160c5d1de9bac76dbb
Lunaticsatoshi <deysarkersudeep@gmail.com>
2021-09-12 19:13:08 +0530

    :triangular_ruler: Updating generate tokens utility

--- miko/src/utils/generateToken.ts
+++ miko/src/utils/generateToken.ts
@@ -2,43 +2,6 @@ import { sign } from "jsonwebtoken";
 import { redisClient } from "../config/redis_connect";
 import { __prod__ } from "../constants";
 
-export const generateAccessToken = (id: string, tokenId: string) => {
-  type payload = {
-    id: string;
-    tokenId: string;
-  };
-  const payload: payload = { id, tokenId };
-
-  return sign(payload, process.env.LINKEDLIST_ACCESS_TOKEN_SECRET!, {
-    expiresIn: __prod__ ? "1h" : "24h",
-  });
-};
-
-export const generateRefreshToken = (id: string) => {
-  type payload = {
-    id: string;
-  };
-  const payload: payload = { id };
-
-  const refreshToken = sign(
-    payload,
-    process.env.LINKEDLIST_REFRESH_TOKEN_SECRET!,
-    {
-      expiresIn: __prod__ ? "30d" : "90d",
-    },
-  );
-
-  redisClient.get(id.toString(), (err) => {
-    if (err) {
-      throw new Error(`❗ Error - ${err.message}`);
-    }
-
-    redisClient.set(id.toString(), JSON.stringify({ token: refreshToken }));
-  });
-
-  return refreshToken;
-};
-
 export const generateTokens = (id: string, tokenId: string) => {
   type payload = {
     id: string;
