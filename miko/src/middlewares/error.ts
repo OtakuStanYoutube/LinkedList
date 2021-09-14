d26205f5d090e2627f4914e351cf745c384f2286
Lunaticsatoshi <deysarkersudeep@gmail.com>
2021-09-14 19:57:30 +0530

    :jigsaw: Updating error middleware to handle errors

--- miko/src/middlewares/error.ts
+++ miko/src/middlewares/error.ts
@@ -13,7 +13,6 @@ export const errorHandler = (
   err: Error,
   _req: Request,
   res: Response,
-  _next: NextFunction,
 ) => {
   const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
 
