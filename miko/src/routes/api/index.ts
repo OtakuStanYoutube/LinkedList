0cfa986d2c48cdff991700332df9fa2f698147a2
Lunaticsatoshi <deysarkersudeep@gmail.com>
2021-09-15 20:37:39 +0530

    :railway_track: Updating api routes

--- miko/src/routes/api/index.ts
+++ miko/src/routes/api/index.ts
@@ -3,4 +3,7 @@ import { router as userRoutes } from "./user";
 
 export const router: Router = Router();
 
+// @desc     All user routes for linkedlist api
+// @route    POST /api/v1/users/
+// @access   Public
 router.use("/user", userRoutes);
