cd14aa7f81abe79dcae1acc9ba39ece7a1d85002
Lunaticsatoshi <deysarkersudeep@gmail.com>
2021-09-16 20:23:56 +0530

    :wrench: Adding create test redis connection configuration

---
+++ miko/src/tests/config/test_redis_connect.ts
@@ -0,0 +1,34 @@
+import { createClient, RetryStrategy, RetryStrategyOptions } from "redis";
+
+const retry_strategy: RetryStrategy = (options: RetryStrategyOptions) => {
+  if (options.error && options.error.code === "ECONNREFUSED") {
+    return new Error(
+      ` ! Error: `.inverse.red.bold + "The server refused the connection",
+    );
+  }
+  if (options.total_retry_time > 1000 * 60 * 60) {
+    // End reconnecting after a specific timeout and flush all commands
+    // with a individual error
+    return new Error(` ! Error: `.inverse.red.bold + "Retry time exhausted");
+  }
+  if (options.attempt > 10) {
+    // End reconnecting with built in error
+    return undefined;
+  }
+  // reconnect after
+
+  return Math.min(options.attempt * 100, 3000);
+};
+
+const redisClient = createClient({
+  url: "redis://redis:6379",
+  retry_strategy,
+});
+
+const connectRedis = () => {
+  redisClient.on("connect", () => {
+    console.log("redis Client Connected");
+  });
+};
+
+export { redisClient, connectRedis };
