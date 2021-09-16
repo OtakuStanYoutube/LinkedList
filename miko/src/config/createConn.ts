d3ef83da1b511e217499be62fbd4ee3711474a24
Lunaticsatoshi <deysarkersudeep@gmail.com>
2021-09-16 19:32:48 +0530

    :hammer: Udating create typeorm configuration

--- miko/src/config/createConn.ts
+++ miko/src/config/createConn.ts
@@ -1,8 +1,18 @@
-import { createConnection, getConnectionOptions } from "typeorm";
-// import { __prod__ } from "../constants";
+import { createConnection } from "typeorm";
+import { __prod__ } from "../constants";
 
-export const createConn = async () => {
-  const conOptions = await getConnectionOptions();
-
-  return createConnection(conOptions);
+export const createConn = () => {
+  return createConnection({
+    type: "postgres",
+    host: "postgres",
+    port: 5432,
+    username: __prod__ ? process.env.DB_USERNAME : "postgres",
+    password: __prod__ ? process.env.DB_PASSWORD : "postgres",
+    database: __prod__ ? process.env.DB_DATABASE : "linkedlist",
+    // url: __prod__ ? process.env.DATABASE_URL : undefined,
+    entities: ["dist/entities/*.js"],
+    migrations: ["dist/migrations/*.js"],
+    synchronize: !__prod__,
+    logging: !__prod__,
+  });
 };
