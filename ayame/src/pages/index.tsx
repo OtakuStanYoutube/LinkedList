1205a09857e5ae1787b76c499047f37a3e78b0f3
Lunaticsatoshi <deysarkersudeep@gmail.com>
2021-11-20 20:15:24 +0530

    :package: fix: Fixing index page layout

--- ayame/src/pages/index.tsx
+++ ayame/src/pages/index.tsx
@@ -1,17 +1,14 @@
 import Hello from "src/components/Hello";
-import Layout from "src/components/shared/Layout";
-import { Wrapper } from "src/components/shared/Wrapper";
+import { Wrapper, Layout } from "src/components/shared";
 
 export default function Index() {
   return (
     <>
-      <div>
-        <Layout title="Home">
-          <Wrapper>
-            <Hello />
-          </Wrapper>
-        </Layout>
-      </div>
+      <Layout title="Home">
+        <Wrapper>
+          <Hello />
+        </Wrapper>
+      </Layout>
     </>
   );
 }
