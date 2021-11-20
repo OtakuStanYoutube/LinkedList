4165456aa174eb68e8228a541ad8b322b4f1047d
Lunaticsatoshi <deysarkersudeep@gmail.com>
2021-11-20 19:49:05 +0530

    :zap: fix: Renaming login page in lower case

--- ayame/src/pages/Login.tsx
+++ ayame/src/pages/Login.tsx
@@ -1,8 +1,6 @@
 import { FC } from "react";
-import { Formik, Form } from "formik";
-
 import { useRouter } from "next/router";
-import NextLink from "next/link";
+// import NextLink from "next/link";
 
 //MUI Components
 // import { Button } from "@material-ui/core";
@@ -10,7 +8,6 @@ import NextLink from "next/link";
 import Layout from "src/components/shared/Layout";
 import { Wrapper } from "src/components/shared/Wrapper";
 import LoginForm from "src/modules/auth/LoginForm";
-import InputField from "src/components/shared/InputField";
 
 const LoginPage: FC<{}> = ({}) => {
   const router = useRouter();
@@ -20,36 +17,6 @@ const LoginPage: FC<{}> = ({}) => {
         <Layout title="Login">
           <Wrapper>
             <LoginForm message="Login" />
-            {/* <Formik
-              initialValues={{ email: "", password: "" }}
-              onSubmit={async (values, { setErrors }) => {
-                console.log(values);
-              }}
-            >
-              {({ isSubmitting }) => (
-                <Form>
-                  <InputField
-                    name="email"
-                    placeholder="lolilover@hentaihaven.org"
-                    label="Email"
-                  />
-                  <InputField
-                    name="password"
-                    placeholder="*******"
-                    label="Password"
-                    type="password"
-                  />
-                  <NextLink href="#">forgot password?</NextLink>
-                  <button
-                    className="w-full px-4 py-2 font-bold text-white bg-blue-500 rounded-full hover:bg-blue-700 focus:outline-none focus:shadow-outline"
-                    disabled={isSubmitting}
-                    type="submit"
-                  >
-                    login
-                  </button>
-                </Form>
-              )}
-            </Formik> */}
           </Wrapper>
         </Layout>
       </div>
